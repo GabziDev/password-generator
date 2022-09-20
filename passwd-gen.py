c7ada65881efbdb80365e9d33a91ef4f0fdb7eb4
GabziDev <108590570+GabziDev@users.noreply.github.com>
2022-09-20 19:02:20 +0200

    Update passwd-gen.py

--- passwd-gen.py
+++ passwd-gen.py
@@ -5,20 +5,16 @@ from string import ascii_letters, digits
 symbols = "/\*&@#$%?!-_"
 
 use = ascii_letters + digits + symbols
-raw_length = input("\n[1] Taille de votre mot de passe [Min:1, Max:74]: >>>")
+raw_length = input("\n[1]  Taille de votre mot de passe [Min:1, Max:65] >> ")
 
 if raw_length.isdigit():
     length = int(raw_length)
-else:
-    print("\n[ /!\ ] Vous n'avez pas saisi un nombre valide [Min:1, Max:74]")
-    time.sleep(2.5)
-    exit()
 
-if 0 < length < 65:
+if 0 < length < 66:
     password = "".join([random.choice(use) for _ in range(length)])
-    print("\n\n[2]  Votre mot de passe :\n\n" + password)
+    print("\n\n[2]  Votre mot de passe :\n\n>>>  " + password)
 else:
-    print("\n[ /!\ ] Vous n'avez pas saisi un nombre valide [Min:1, Max:74]")
+    print("\n[ /!\ ] Vous n'avez pas saisi un nombre valide [Min:1, Max:65]")
     time.sleep(2.5)
     exit()
 #github
